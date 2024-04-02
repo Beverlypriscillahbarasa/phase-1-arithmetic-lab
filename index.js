8731f061959f462890a100d732c40ee6c6409af1
priscillah barasa <beverlybarasa@gmail.com>
2024-04-02 15:48:17 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 
-// Write your code here
+// Declare and initialize num1 and num2
+let num1 = 31;
+let num2 = 2;
+
+// Create a variable called multiply and assign it the result of num1 * num2
+let multiply = num1 * num2;
+
+// Check if the result of multiplication is equal to 62
+//if (multiply === 62) 
+    console.log("multiply");
+
+let random = 15;
+let num3 = 16;
+let num4 =4;
+
+let mod = num3 / num4
+    console.log("mod");
+
+
+let numbers = [10,14,15,17,20];
+let max = Math.max(...numbers);
+
+// Output the maximum number
+console.log("The highest number in the set is:", max); // Output: The highest number in the set is: 20
+
+
+
+
